f92e3adb59ccdfc255b873e4b9b2c65e08c06fc1
Vitor Riboli <vitorriboli1@gmail.com>
2022-06-09 14:04:06 -0300

    Finalização

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function getTimeSecond(second) {  //criar os segundos e retornar-los
     const data = new Date(second * 1000); //JS recebe me mileseimos de segundos, * 1000, retorna o segundo
-    
+
     return data.toLocaleTimeString('pt-BR', {
         hour12: false,
         timeZone: 'GMT' //deixou a hora zerada, pode ser GMT ou UTC, pode olhar na wike sobre
@@ -19,28 +19,23 @@ function startTimer() {  //função para iniciar o timer
         second++; //a cada segundo adiciona um
         timer.innerHTML = getTimeSecond(second);
     }, 1000)
-
     iniciar.innerHTML = 'Iniciar'
 }
 
-iniciar.addEventListener('click', function(event){
-
+iniciar.addEventListener('click', function (event) {
+    clearInterval(watchTimer)
     startTimer();
-    //timer.innerHTML = ''
     timer.style.color = 'white'
 })
 
-pausar.addEventListener('click', function(event){
-    
+pausar.addEventListener('click', function (event) {
     clearInterval(watchTimer);
-
     timer.style.color = 'red'
     iniciar.innerHTML = 'Continuar'
     iniciar.style.textAlign = 'center'
 })
 
-zerar.addEventListener('click', function(event){
-    
+zerar.addEventListener('click', function (event) {
     clearInterval(watchTimer);
     timer.innerHTML = '00:00:00'
     timer.style.color = 'green'
